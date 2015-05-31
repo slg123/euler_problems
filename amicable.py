226018200806937fb28685b6a5ae750af5492c00
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2015-05-31 08:05:05 -0700

    proj euler num 21 - amicable factors under 10000

---
+++ amicable.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+def factor_sum(n):
+    f = [i for i in range(1, n//2 + 1) if not n % i] + [n]
+    return sum(f) - n
+
+A = []
+def make_amicable_list(n):
+    k = factor_sum(n)
+    j = factor_sum(k)
+    if j == n and n != k:
+        A.append(n)
+
+for i in range(10002):
+    make_amicable_list(i)
+
+print sum(A)
+
+
