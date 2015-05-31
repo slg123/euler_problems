d89ce22e7ad9428f16e4e0a93dff29b27ef1e76a
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2015-05-31 16:22:07 -0700

    euler problem 22 in perl.

--- n.pl
+++ n.pl
@@ -1,5 +1,4 @@
 #!/usr/bin/perl
-
 #
 # Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
 #
