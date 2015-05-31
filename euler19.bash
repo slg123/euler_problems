e281a8d29f2f0e009cad8dff9ea28dffdf82dd5f
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2015-05-30 19:55:28 -0700

    euler19 - num sundays on first of month 1901 - 2000

--- euler19.bash
+++ euler19.bash
@@ -1,17 +1,17 @@
 #!/usr/bin/bash
-i=1901
+y=1901
 num_sundays=0
-while [ $i -le 2000 ]; do
-    j=1
+while [ $y -le 2000 ]; do
+    m=1
     echo "working on year $i..."
-    while [ $j -le 12 ]; do
-        val=`cal $j $i | awk -F, '{sum += $1} END {print sum}'`
+    while [ $m -le 12 ]; do
+        val=`cal $m $y | awk -F, '{sum += $1} END {print sum}'`
         if [ $val -eq 46 -o $val -eq 75 ]; then
             let num_sundays=$num_sundays+1
         fi
-        let j=$j+1
+        let m=$m+1
     done
-    let i=$i+1
+    let y=$y+1
 done
 echo "$num_sundays sundays fall on the first of the month" 
 echo "from 1 Jan 1901 to 31 Dec 2000"
