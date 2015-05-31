f4560130709610d5dd993e4a47aac22438b4ac32
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2015-05-31 12:50:49 -0700

    euler problem 22 in perl.

--- n.pl
+++ n.pl
@@ -1,17 +1,24 @@
 #!/usr/bin/perl
 
+#
+# Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over five-thousand first names, begin by sorting it into alphabetical order. Then working out the alphabetical value for each name, multiply this value by its alphabetical position in the list to obtain a name score.
+#
+#
+# For example, when the list is sorted into alphabetical order, COLIN, which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+#
+# What is the total of all the name scores in the file?
+#
+
 use strict;
 use warnings;
 
 my @n = (); 
-
 my $f = 'names.txt';
 open my $fh, '<', $f or die 'cannot open: $!';
 while (<$fh>) {
     my @fields = split(',', $_);
     for (@fields) {
-        s/\"//; 
-        s/\"//; 
+        s/\"//; s/\"//; 
         push @n, $_; 
     }
 }
