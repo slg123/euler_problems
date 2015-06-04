f1236f7e9ca85df640aa1e18134921becb854f42
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2015-06-04 14:40:55 -0700

    euler 25 python

---
+++ e25.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import functools
+
+@functools.lru_cache(maxsize=None)
+def fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+
+for i in range(0,10000):
+    n = fibonacci(i)
+    if len(str(n)) == 1000:
+        print("term: "+str(i))
+        break
+
