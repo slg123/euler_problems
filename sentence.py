0ebd94aa405200760067809dbc373db55b122771
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2016-03-07 20:38:40 -0600

    find letters not in a sentence, return array of unused letters

--- sentence.py
+++ sentence.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-sentence = 'this is a sentence'
+sentence = 'this is a sentence with quite a few letters in it, look, a zebra!'
 absent = []
 for i in alphabet:
     if i in alphabet and i not in sentence:
