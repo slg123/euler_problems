2d89a49202c42daf1b0d0c301840918565d15c98
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2016-03-12 16:26:21 -0600

    c qsort with comparison function example

---
+++ sort.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int cmpfunc(const void *a, const void *b); 
+
+int main() {
+
+    int a[] = { 0, 1, 2, 3, 4, 5 };
+    int b[] = { 3, 0, 99, 1, 7, -10 };
+
+    printf("%d\n", sorted(a)); 
+    printf("%d\n", sorted(b)); 
+
+    int sz = sizeof(b) / sizeof(int); 
+    printf("%d\n", sz); 
+
+    print_array(a, sz); 
+    print_array(b, sz); 
+
+    qsort(b, sz, sizeof(int), cmpfunc); 
+
+    print_array(b, sz); 
+
+    return 0;
+}
+
+int cmpfunc(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b); 
+}
+
+
+int sorted(int ar[]) {
+    int i;
+    int n = sizeof(ar)/sizeof(ar[0]);
+
+    for (i=0; i<n-1; i++) {
+        if (ar[i] > ar[i+1]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int print_array(int ar[], int sz) {
+    int i;
+    for (i=0; i < sz; i++) {
+        printf(" %d ", ar[i]); 
+    }
+    printf("\n"); 
+}
