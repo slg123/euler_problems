c0ec01d6cf909f96802daa3d4fa0db4eee0dc1b2
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2016-03-12 07:47:05 -0600

    lol so many pretty colors man

--- javascript/trap.js
+++ javascript/trap.js
@@ -16,6 +16,27 @@ function sum(n) {
     return v;
 }
 
+//function reduce(combine, base, array) {
+//    forEach(array, function(element) {
+//        base = combine(base, element); 
+//    });
+//    return base; 
+//}
+//
+//function add(a, b) {
+//    return a+b; 
+//}
+//
+//function sum(numbers) {
+//    return reduce(add, 0, numbers); 
+//}
+//
+//function forEach(array, action) {
+//    for (var i=0; i<array.length; i++) 
+//        action(array[i]);
+//}
+
+
 function trapezium(f, x, h) {
     return (f(x) + f(x+h))/2.0;
 }
@@ -39,5 +60,3 @@ function print(obj) {
 
 var result = integrate(f, 1, Math.PI, 100, trapezium)
 print(result); 
-
-
