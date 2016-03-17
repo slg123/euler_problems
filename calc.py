f57e3af6ea4e1a89fb4cb049220bb97795be3451
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2016-03-17 08:17:53 -0500

    basic calc - derivatives and integrals in python from python mailing list

---
+++ calc.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# derivative fxn
+def D(f, h=1e-3):
+    def df(x):
+        derive=(f(x+h)-f(x))/h
+        return round(derive, 3)
+    return df
+
+def g(x):
+    return x*x
+
+N = []
+N.append([g(x) for x in range(11)])
+print N
+
+# definite integral fxn
+def I(f, h=1e-3):
+    def intf(b, a=0):
+        sum=0
+        x=a
+        while x <= b:
+            sum += h*(f(x+h)+f(x))/2.0
+            x += h
+        return round(sum, 3)
+    return intf
+
+inv1 = I(D(g))
+M = []
+M.append([inv1(x) for x in range(11)])
+print M
+
+
