a1c000391da4390f4b4648994e16684aa948a44c
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2016-03-19 18:26:20 -0500

    logic gate messin around with c++ toy code

--- logicGate.cpp
+++ logicGate.cpp
@@ -6,6 +6,7 @@ class LogicGate {
     public:
         int doAndGateOperation(int a, int b); 
         int doOrGateOperation(int a, int b); 
+        int doNorGateOperation(int a, int b); 
 };
 
 LogicGate::doAndGateOperation(int a, int b) {
@@ -21,11 +22,49 @@ LogicGate::doAndGateOperation(int a, int b) {
 LogicGate::doOrGateOperation(int a, int b) {
     int cmpa = a;
     int cmpb = b;
-    if ((cmpa==1) || (cmpb == 1)) {
+    if ((cmpa == 1) || (cmpb == 1)) {
         return 1;
     } else {
         return 0;
     }
+}
+
+LogicGate::doNorGateOperation(int a, int b) {
+    int cmpa = a;
+    int cmpb = b;
+    if ((cmpa == 0) && (cmpb == 0)) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+void testGates() {
+
+    LogicGate andGate;
+    LogicGate orGate;
+    LogicGate norGate;
+
+    int val_a[4] = { 0, 0, 1, 1 };
+    int val_b[4] = { 0, 1, 0, 1 };
+    
+    for (int i=0; i<4; i++) {
+        cout << "AND gate: " << val_a[i] << " " << val_b[i] << " :" << 
+                andGate.doAndGateOperation(val_a[i], val_b[i]) << endl;
+    }
+    cout << "\n\n\n"; 
+
+    for (int i=0; i<4; i++) {
+        cout << "OR  gate: " << val_a[i] << " " << val_b[i] << " :" <<
+                orGate.doOrGateOperation(val_a[i], val_b[i]) << endl;
+    }
+    cout << "\n\n\n"; 
+
+    for (int i=0; i<4; i++) {
+        cout << "NOR gate: " << val_a[i] << " " << val_b[i] << " :" << 
+                norGate.doNorGateOperation(val_a[i], val_b[i]) << endl;
+    }
+
 }
 
 int main() 
@@ -35,12 +74,18 @@ int main()
 
     LogicGate andGate;
     LogicGate orGate; 
+    LogicGate norGate; 
 
     int and_result = andGate.doAndGateOperation(a, b); 
-    int or_result = orGate.doOrGateOperation(a, b); 
+    int or_result  = orGate.doOrGateOperation(a, b); 
+    int nor_result = norGate.doNorGateOperation(a, b);
 
     cout << and_result << endl;
     cout << or_result << endl; 
+    cout << nor_result << endl; 
+
+    /* test all gates */
+    testGates(); 
 
     return 0;
 }
