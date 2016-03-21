f0a6c73c2b4870e139de525962296aea0b646449
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2016-03-20 19:58:23 -0500

    project euler number 22 in python yippee this took way too long lol

--- e22.py
+++ e22.py
@@ -5,17 +5,16 @@ NAMES = open('names.txt').read().split(',')
 SORTED = sorted(NAMES)
 
 def sum_word_value(n):
-    total = 0
+    sum = 0
     for i in n:
         if i.isalpha():
-            total += ord(i)-64
-    return total
+            sum += ord(i)-64
+    return sum
 
 position = 0
-grand_total = 0
+total = 0
 for i in SORTED:
     position += 1
-    grand_total += position * sum_word_value(i)
+    total += position * sum_word_value(i)
 
-print grand_total
-
+print total
