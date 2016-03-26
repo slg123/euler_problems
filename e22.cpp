3e20340bdb4a40c8fc5033962ee88ae645228600
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2016-03-25 20:27:48 -0500

    project euler problem 22 in c++ using modern c++

--- e22.cpp
+++ e22.cpp
@@ -9,8 +9,7 @@ using namespace std;
 
 int get_numeric_value(string s);
 
-int main() 
-{
+int main() {
     ifstream infile("names.txt"); 
     string line = ""; 
     vector<string> all_words;
@@ -24,8 +23,7 @@ int main()
     sort(all_words.begin(), all_words.end()); 
 
     int position = 0;
-    int total = 0; 
-
+    int total = 0;
     for (unsigned i=0; i < all_words.size(); i++) {
         position++; 
         total += position * get_numeric_value(all_words[i]); 
@@ -34,8 +32,7 @@ int main()
     return 0;
 }
 
-int get_numeric_value(string s) 
-{
+int get_numeric_value(string s) {
     int total = 0; 
     for (unsigned i : s) {
         if (i > 64) { 
