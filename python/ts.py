44d45de66c90f34b0d3e889a21a3d253716c57bf
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2016-03-27 10:41:34 -0500

    time some things

--- python/ts.py
+++ python/ts.py
@@ -22,6 +22,8 @@ def one():
     for i in range(10):
         print("Sum is %d required %10.7f seconds"%sumOfN(10000000))
 
+    print("\n\n\n"); 
+
 #
 #     n
 #    E    (n)(n+1)
@@ -35,6 +37,8 @@ def two():
 
     for i in range(10):
         print("Sum is %d required %10.7f seconds"%sumOfN2(10000000))
+
+    print("\n\n\n"); 
          
 
 #
@@ -51,9 +55,39 @@ def three():
     for i in range(10):
         print("Sum is %d required %10.7f seconds"%sumOfN3(10000000))
 
+    print("\n\n\n"); 
+
+#
+#     n    2
+#    E    n
+#     i=1 
+#        
+#
+def four():
+    def nSquared(n):
+        i = 0
+        start = time.time()
+        j = 5 * (n**2) + 27*n + 1005
+        i = (n**2)
+        end = time.time()
+        return i, end-start
+
+    for i in range(10):
+        print("Sum is %d required %10.7f seconds"%nSquared(10000000))
+
+    print("\n\n\n")
+
+    for j in range(10):
+        print("Sum is %d required %10.7f seconds"%nSquared(10000000))
+
+    print(nSquared(2)); 
+    print(nSquared(3)); 
+    print(nSquared(4)); 
+    
 one()
 two()
 three()
+four()
 
 
 
