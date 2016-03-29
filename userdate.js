41e7870fdabaa9d01cff84f82a072abe4ddeb35f
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2016-03-29 18:22:45 -0500

    return a yyyymmdd date from a numeric date like 1/2/2001 or 01/12/2016

--- userdate.js
+++ userdate.js
@@ -1,12 +1,17 @@
 function formatDate(userDate) {
     var date = userDate.split("/"); 
-    m = date[0], d = date[1], y = date[2];
+
+    m = date[0];
+    d = date[1];
+    y = date[2];
+
     function fixDate(s) {
         if (s.length < 2) {
             return 0+s;
         }
         return s;
     }
+
     m = fixDate(m); 
     d = fixDate(d);
     return String(y+m+d); 
