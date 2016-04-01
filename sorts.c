1d395b6bf115275188eb9bd6e93f17f9ba72c0a3
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2016-04-01 17:57:53 -0500

    timing bubble & shell sorts in c

--- sorts.c
+++ sorts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define NELEMS 20
+#define NELEMS 10000
+#define NUM_SORTS 10000
 
+void initialize_array(int *a, int sz); 
 void generate_array(int *a, int sz);
 void bubble_sort(int *ar);
 void selection_sort(int *ar);
@@ -11,33 +14,57 @@ void display(int *ar);
 
 int main() {
 
+    clock_t begin_bubblesort, end_bubblesort;
+
+    printf("running %d sort operations of %d array elements using bubble sort...\n", NUM_SORTS, NELEMS); 
+
+    begin_bubblesort = clock();
     int arr[NELEMS];
+    for (int i=0; i<NUM_SORTS; i++) {
+        generate_array(arr, NELEMS); 
+        bubble_sort(arr);
+    }
+    end_bubblesort = clock(); 
 
-    generate_array(arr, NELEMS); 
-    display(arr); 
-    bubble_sort(arr);
+    printf("%d %d element sorts took %.3f seconds.\n", NUM_SORTS, NELEMS, 
+        (double)(end_bubblesort - begin_bubblesort)/CLOCKS_PER_SEC); 
+
     display(arr); 
 
+    clock_t begin_selection_sort, end_selection_sort;
+
+    printf("running %d sort operations of %d array elements using selection sort...\n", NUM_SORTS, NELEMS); 
+
+    begin_selection_sort = clock();
     int arr2[NELEMS];
+    for (int i=0; i<NUM_SORTS; i++) {
+        generate_array(arr2, NELEMS); 
+        selection_sort(arr2);
+    }
+    end_selection_sort = clock();
 
-    generate_array(arr2, NELEMS); 
-    display(arr2);
-    selection_sort(arr2);
-    display(arr2);
+    printf("%d %d element sorts took %.3f seconds.\n", NUM_SORTS, NELEMS, 
+        (double)(end_selection_sort - end_selection_sort)/CLOCKS_PER_SEC); 
+
+    display(arr2); 
 
     return 0;
 }
 
+void initialize_array(int *a, int sz) {
+    for (int i=0; i<sz; i++) {
+        a[i] = 0;
+    }
+}
+
 void generate_array(int *a, int sz) {
-    int i;
-    for (i=0; i<sz; i++) {
+    for (int i=0; i<sz; i++) {
         a[i] = rand() % 99 + 1; 
     }
 }
 
 void bubble_sort(int *a) {
-    int i;
-    int j;
+    int i, j;
     for (i=NELEMS-1; i>1; i--) {
         for (j=0; j<i; j++) {
             if (a[j] > a[j+1]) {
@@ -48,9 +75,7 @@ void bubble_sort(int *a) {
 }
 
 void selection_sort(int *a) {
-    int i;
-    int j;
-    int min;
+    int i, j, min;
     for (i=0; i<NELEMS-1; i++) {
         min = i;
         for (j=i+1; j<NELEMS; j++) {
@@ -68,9 +93,9 @@ void swap(int i, int j, int *a) {
     a[j] = t;
 }
 
+// display last 20 elements in sorted array, if desired.
 void display(int *a) {
-    int i;
-    for (i=0; i<NELEMS; i++) {
+    for (int i=NELEMS-20; i<NELEMS; i++) {
         printf("%d ", a[i]);
     }
     printf("\n"); 
