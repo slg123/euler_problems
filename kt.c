e6ef0729e4b615ec18ce27f176784834714d643b
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2016-04-02 21:43:09 -0500

    starting a knights tour in c

--- kt.c
+++ kt.c
@@ -5,6 +5,11 @@ typedef struct {
     int y;
 } knight_move;
 
+const int N = 8;
+
+void initialize_board(int a[N][N]); 
+void print_board(int a[N][N]);
+
 int main() {
 
     knight_move moves[] = { 1,2, 2,1, 1,-2, 2,-1, -1,2, -2,1, -1,-2, -2,-1 };
@@ -12,6 +17,29 @@ int main() {
     for (int i=0; i<sizeof(moves)/sizeof(moves[0]); i++) {
         printf("%d %d\n", moves[i].x, moves[i].y); 
     }
+ 
+    int ar[N][N]; 
+
+    initialize_board(ar);
+    print_board(ar); 
 
     return 0;
+}
+
+void initialize_board(int a[N][N]) {
+    for (int i=1; i<=8; i++) {
+        for (int j=1; j<=8; j++) {
+            a[i][j] = 0;
+        }
+    }
+}
+
+void print_board(int a[N][N]) {
+    for (int i=1; i<=8; i++) {
+        for (int j=1; j<=8; j++) {
+            printf(" %d", a[i][j]); 
+        }
+        printf("\n"); 
+    }
+    printf("\n\n"); 
 }
