4902a33688abcd01db12c42c7901ad0179d6064a
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2016-04-27 19:48:40 -0400

    adding go Knights tour - ./KnightsTour | head -8

--- Knights_Tour/KnightsTour.go
+++ Knights_Tour/KnightsTour.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
+//
+// Knights Tour - walk a knight piece along a solved tour of an 8x8 chessboard.
+//
+// example usage: 
+//     ./KnightsTour | head -8
+//
+//      0 59 38 33 30 17  8 63
+//     37 34 31 60  9 62 29 16
+//     58  1 36 39 32 27 18  7
+//     35 48 41 26 61 10 15 28
+//     42 57  2 49 40 23  6 19
+//     47 50 45 54 25 20 11 14
+//     56 43 52  3 22 13 24  5
+//     51 46 55 44 53  4 21 12
+//
+
+
 // The size of the chessboard is NxN. 
 const N = 8
 
