ca5f05e98a9c68b305577143aa7e46245cab1afe
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2016-04-29 22:05:02 -0400

    starting a check 'checker' program... check to see if a b or w king is in check, from Programming Challenges, Skiena, et al

---
+++ Knights_Tour/cc.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+
+#define N 8
+
+void initialize_board(char board[N][N]);
+void print_board(char board[N][N]); 
+void display_queen_moves(char board[N][N]); 
+void to_algebraic(char board[N][N]);
+
+int main() {
+    char board[N][N]; 
+
+    int queen_xmoves[] = { 1, 2, 3, 4, 5, 6, 7, 8, -1, -2, -3, -4, -5, -6, -7, -8 };
+    int queen_ymoves[] = { 1, 2, 3, 4, 5, 6, 7, 8, -1, -2, -3, -4, -5, -6, -7, -8 }; 
+
+    initialize_board(board); 
+    board[3][3] = 'q'; 
+    print_board(board); 
+    display_queen_moves(board); 
+    printf("\n"); 
+    to_algebraic(board); 
+
+    return 0;
+}
+
+void to_algebraic(char board[N][N]) {
+    int i, j;
+    for (i=0; i<N; i++) {
+        for (j=0; j<N; j++) {
+            printf("%d%d", i, j); 
+        }
+        printf("\n"); 
+    }
+}
+
+void display_queen_moves(char board[N][N]) {
+    int i, j;
+    for (i=0; i<N; i++) {
+        for (j=0; j<N; j++) {
+            if (board[i][j] == 'q') {
+                printf("\nqueen is located at square: %d %d\n", i, j); 
+            }
+        }
+    }
+}
+
+void initialize_board(char board[N][N]) {
+    int i, j;
+    for (i=0; i<N; i++) {
+        for (j=0; j<N; j++) {
+            board[i][j] = '.';
+        }
+    }
+}
+
+void print_board(char board[N][N]) {
+    int i, j;
+    for (i=0; i<N; i++) {
+        for (j=0; j<N; j++) {
+            printf("%c", board[i][j]); 
+        }
+        printf("\n"); 
+    }
+    printf("\n"); 
+}
