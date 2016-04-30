102ad486f4c7325960f7f348d00da7b001e1b826
Scott Gillespie <scott_layne_gillespie@hotmail.com>
2016-04-29 22:54:10 -0400

    starting a check 'checker' program... check to see if a b or w king is in check, from Programming Challenges, Skiena, et al

--- Knights_Tour/cc.c
+++ Knights_Tour/cc.c
@@ -20,25 +20,34 @@ int main() {
     display_queen_moves(board); 
     printf("\n"); 
     to_algebraic(board); 
+    printf("\n\n"); 
 
     return 0;
 }
 
 void to_algebraic(char board[N][N]) {
 
-    char ranks[N] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
+    char ranks[N] = { 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a' };
     char files[N] = { '1', '2', '3', '4', '5', '6', '7', '8' };
 
-    int i, j, s;
-    for (i=1, s=N; i<N, s>=0; i++, s--) {
-        for (j=0; j<N; j++) { 
-            if (i>1) {
-                printf("%c%c", ranks[s], files[j]); 
-            }
+    int i, j;
+    for (i=0; i<N; i++) {
+        for (j=0; j<N; j++) {
+            printf("%c%c", ranks[i], files[j]); 
         }
         printf("\n"); 
     }
-    printf("\n\n"); 
+
+    //int i, j, s, k;
+    //for (i=1, s=N; i<N, s>=0; i++, s--) {
+    //    for (j=0; j<N; j++) { 
+    //        if (i>1) {
+    //            printf("%c%c", ranks[s], files[j]); 
+    //        }
+    //    }
+    //    printf("\n"); 
+    //}
+
 }
 
 void display_queen_moves(char board[N][N]) {
