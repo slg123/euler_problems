f1cef74bc26dd6dc45ce87c5757f8c4306d255ed
Scott Gillespie <scott.layne.gillespie@gmail.com>
2016-05-22 18:06:27 -0600

    pascal's triangle, first 20 rows, no pretty print

--- pascal.py
+++ pascal.py
@@ -12,12 +12,29 @@ for i in range(n):
         this_row.append(last_row[j]+last_row[j+1])
     this_row.append(1)
     print this_row
-    
-    
-    
-        
+
+""" adapted from the following perl:
+
+sub ton_pascal {
+    my $rows = shift;
+    my $last_row = [];
+    my $this_row = [1];
+
+    last unless ($rows > 0);
+    print "1\n";   # first row
+    for (my $i=1; $i<$rows; $i++) {
+        $last_row = $this_row;
+	$this_row = [1];
+	for (my $j=1; $j<$i; $j++) {
+	    push(@$this_row, $last_row->[$j-1]+$last_row->[$j]);
+	}
+	push(@$this_row, 1);
+	print join(' ',@$this_row)."\n";
+    }
+}
+
+"""
 
 
 
 
-    
