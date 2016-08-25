b7b85404bbe4927cc66b8a5b4fb7d1cdffc28eaa
scott <scott.gillespie@r1soft.com>
2016-08-24 19:23:59 -0500

    update easter calc

--- easter.py
+++ easter.py
@@ -7,22 +7,25 @@ import datetime
 """ Meeus / Jones / Butcher algorithm             """
 """ From Practical Astronomy With Your Calculator """
 
-Y = input('Enter the year: ')
-a = Y % 19
-b = floor(Y/100)
-c = Y % 100
-d = floor(b/4)
-e = b % 4
-f = floor((b+8)/25)
-g = floor((b-f+1)/3)
-h = (19*a+b-d-g+15)%30
-i = floor(c/4)
-k = c % 4
-L = (32+2*e+2*i-h-k)%7
-m = floor((a+11*h+22*L)/451)
-month = int ( floor((h+L-7*m+114)/31) )
-day = int ( ((h+L-7*m+114) % 31) + 1 )
+def Gregorian(Y):
+    a = Y % 19
+    b = floor(Y/100)
+    c = Y % 100
+    d = floor(b/4)
+    e = b % 4
+    f = floor((b+8)/25)
+    g = floor((b-f+1)/3)
+    h = (19*a+b-d-g+15)%30
+    i = floor(c/4)
+    k = c % 4
+    L = (32+2*e+2*i-h-k)%7
+    m = floor((a+11*h+22*L)/451)
+    month = int ( floor((h+L-7*m+114)/31) )
+    day = int ( ((h+L-7*m+114) % 31) + 1 )
 
-monthname = datetime.date(1900, month, 1).strftime('%B')
+    monthname = datetime.date(1900, month, 1).strftime('%B')
 
-print str(day)+" "+str(monthname)
+    print str(month) + " " + str(day)
+
+for i in range(2000,2050):
+    Gregorian(i)
