695b7192202a0931b4bd1c8011774efc880c5de8
scott <scott.gillespie@r1soft.com>
2016-08-24 20:19:47 -0500

    cleanup date of easter output, add Julian calc

--- easter.py
+++ easter.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
-from math import *
+from math import floor
 import datetime
+import sys
 
 """ calculate the date of easter                  """
 """ Meeus / Jones / Butcher algorithm             """
@@ -20,12 +21,31 @@ def Gregorian(Y):
     k = c % 4
     L = (32+2*e+2*i-h-k)%7
     m = floor((a+11*h+22*L)/451)
-    month = int ( floor((h+L-7*m+114)/31) )
-    day = int ( ((h+L-7*m+114) % 31) + 1 )
+    month = int(floor((h+L-7*m+114)/31))
+    day = int(((h+L-7*m+114) % 31) + 1)
 
-    monthname = datetime.date(1900, month, 1).strftime('%B')
+    return month, day
 
-    print str(month) + " " + str(day)
+def Julian(Y):
+    a = Y % 4
+    b = Y % 7
+    c = Y % 19
+    d = (19*c + 15) % 30
+    e = (2*a + 4*b - d + 34) % 7
+    f = d + e + 114
+    f, g = f/31, f%31
 
-for i in range(2000,2050):
-    Gregorian(i)
+    return f, g+1
+
+def main():
+    for i in range(1000, 2050):
+        m, d = Gregorian(i)
+        mj, dj = Julian(i)
+        print "Gregorian: " + str(i) + " " + str(m) + " " + str(d) + "\tJulian: " +  str(i) + " " + str(mj) + " " + str(dj)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit()
+        
