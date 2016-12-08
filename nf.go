0e6492f66594983675b0bbf6a16148dc890e8243
scott <scott.gillespie@r1soft.com>
2016-12-08 08:31:49 -0600

    count to 100 without loops or recursion - golang

---
+++ nf.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func a(j int) int {
+	j++
+	fmt.Printf("%d\n", j)
+	return j
+}
+
+func b(j int) int {
+	return a(a(a(a(a(j)))))
+}
+
+func c(n int) int {
+	return b(b(b(b(n))))
+}
+
+func main() {
+	c(c(c(c(c(0)))))
+}
