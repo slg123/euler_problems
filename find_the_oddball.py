74d942440114bc999fdc889f4cb165e37bc70ccc
U-MSI\scott <scott.layne.gillespie@gmail.com>
2017-02-25 16:15:25 -0600

    and in python, why not?

---
+++ find_the_oddball.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+a = [ 10, 10, 20, 20, 20, 30, 30, 9, 9, 8, 8, 7, 7, 4, 4 ]
+
+def get_num(n, *a):
+    count = 0
+    for i in a:
+        if n == i:
+            count += 1
+    return count
+
+
+wanted = 0
+for i in set(a):
+    n = get_num(i, *a)
+    if n % 2 != 0:
+        wanted = i
+    
+
+print wanted
+    
