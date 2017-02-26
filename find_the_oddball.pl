67b701fb4673a2557cf1f2f74150b6cd03f922fb
U-MSI\scott <scott.layne.gillespie@gmail.com>
2017-02-25 18:30:45 -0600

    find the numbers, not just one, that show up an odd number of times in an array

--- find_the_oddball.pl
+++ find_the_oddball.pl
@@ -1,8 +1,7 @@
 #!/usr/bin/perl
 
 # You have an array of numbers. 
-# Each number but one appears an even number of times. 
-# Identify the number that appears an odd number of times. 
+# Identify the number(s) that appear an odd number of times. 
 
 my @a = ( 10, 10, 20, 20, 30, 30, 30, 10, 10, 9, 9, 8, 8, 7, 7, 6, 6, 4, 4, 4 ); 
 
